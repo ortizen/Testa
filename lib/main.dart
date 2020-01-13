08fa1ef63f8010ab6939a4fbcbf40a819b9cc9db
ortizen <oice67@gmail.com>
2020-01-13 17:03:04 -0600

    First pass animation with hourglass

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: RotatingBall(),
@@ -39,12 +29,11 @@ class _RotatingBallState extends State<RotatingBall>
   void initState() {
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 10),
-    )..addListener(() => setState(() {}));
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.decelerate,
-    );
+      duration: Duration(seconds: 15),
+    )
+      ..repeat()
+      ..addListener(() => setState(() {}));
+
     super.initState();
     _controller.forward();
   }
@@ -57,13 +46,23 @@ class _RotatingBallState extends State<RotatingBall>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: RotationTransition(
-        turns: _animation,
-        child: Transform(
-            transform: Matrix4.translationValues(50.0, 50.0, 0.0),
-            child: Image.asset('assets/images/ball.png')),
-      ),
+    return Stack(
+      children: [
+        RotationTransition(
+          turns: _animation,
+          alignment: Alignment.center,
+          child: Image.asset('assets/images/ball.png'),
+        ),
+        ScaleTransition(
+          alignment: Alignment.center,
+          scale: _controller,
+          child: ScaleTransition(
+            alignment: Alignment.center,
+            scale: _controller,
+            child: Image.asset(''),
+          ),
+        ),
+      ],
     );
   }
 }
